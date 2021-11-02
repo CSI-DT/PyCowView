e9037fb09905837399b4b27f25388acc4890096e
Mikhail Churakov <17121916+mc30@users.noreply.github.com>
2021-11-02 15:39:31 +0100

    Added functions from previous repo

---
+++ pycowview/__init__.py
@@ -0,0 +1,8 @@
+"""
+Python package to work with real-time location system (RTLS) data from GEA CowView system.
+"""
+
+# import .functions
+# from .data_io import csv_read_FA
+
+__version__ = "0.1.0"
